de2827b31bd8a4d84a27527c61049952185d32e1
Drew <akeller@eight-eleven.com>
2015-03-14 14:19:01 -0400

    esetimate form

--- estimates.php
+++ estimates.php
@@ -10,6 +10,13 @@ Fine Art Storage, Climate Controlled storage, Fine Art Transportation, Museum Qu
 Trained Professionals providing Fine Art Storage, Fine Art Shipping, Fine Art Transportation, fine art packing, fine art crating,  rigging of fine art, artifacts, and antiques in San Francisco CA California, and Denver, CO Colorado and Aspen Colorado.
 "/>
 
+<!-- Wufoo -->
+<link href="css/structure.css" rel="stylesheet">
+<link href="css/form.css" rel="stylesheet">
+
+<!-- Wufoo -->
+<script src="scripts/wufoo.js"></script>
+
 <?php include 'includes/header.php'; ?>
 
 <body class="page-inside one-col">
@@ -38,9 +45,284 @@ Trained Professionals providing Fine Art Storage, Fine Art Shipping, Fine Art Tr
 	<div class="container-common">
 		<div class="main">
 
-<p>Call the following for an estimate:<br />
-  (650) 952-0100 - San Francisco<br />
-  (303) 291-3906 - Denver</p>
+<form id="form1" name="form1" class="wufoo topLabel page" accept-charset="UTF-8" autocomplete="off" enctype="multipart/form-data" method="post" novalidate
+      action="https://shipart.wufoo.com/forms/zjq2vmg0cvx6bq/#public">
+  
+<header id="header" class="info">
+<h2>Estimate</h2>
+<div></div>
+</header>
+
+<ul>
+
+<li id="foli9" class="notranslate      ">
+<label class="desc" id="title9" for="Field9">
+Name
+<span id="req_9" class="req">*</span>
+</label>
+<span>
+<input id="Field9" name="Field9" type="text" class="field text fn" value="" size="8" tabindex="1"       required />
+<label for="Field9">First</label>
+</span>
+<span>
+<input id="Field10" name="Field10" type="text" class="field text ln" value="" size="14" tabindex="2" required />
+<label for="Field10">Last</label>
+</span>
+</li>
+<li id="foli11" class="notranslate      ">
+<label class="desc" id="title11" for="Field11">
+Street Address
+</label>
+<div>
+<input id="Field11" name="Field11" type="text" class="field text medium" value="" maxlength="255" tabindex="3" onkeyup=""       />
+</div>
+</li>
+<li id="foli12" class="notranslate      ">
+<label class="desc" id="title12" for="Field12">
+State
+<span id="req_12" class="req">*</span>
+</label>
+<div>
+<input id="Field12" name="Field12" type="text" class="field text small" value="" maxlength="255" tabindex="4" onkeyup=""       required />
+</div>
+</li>
+<li id="foli13" class="phone notranslate      ">
+<label class="desc" id="title13" for="Field13">
+Phone Number
+</label>
+<span>
+<input id="Field13" name="Field13" type="tel" class="field text" value="" size="3" maxlength="3" tabindex="5"       />
+<label for="Field13">###</label>
+</span>
+<span class="symbol">-</span>
+<span>
+<input id="Field13-1" name="Field13-1" type="tel" class="field text" value="" size="3" maxlength="3" tabindex="6" />
+<label for="Field13-1">###</label>
+</span>
+<span class="symbol">-</span>
+<span>
+ <input id="Field13-2" name="Field13-2" type="tel" class="field text" value="" size="4" maxlength="4" tabindex="7" />
+<label for="Field13-2">####</label>
+</span>
+</li>
+<li id="foli14" class="phone notranslate      ">
+<label class="desc" id="title14" for="Field14">
+Fax Number
+</label>
+<span>
+<input id="Field14" name="Field14" type="tel" class="field text" value="" size="3" maxlength="3" tabindex="8"       />
+<label for="Field14">###</label>
+</span>
+<span class="symbol">-</span>
+<span>
+<input id="Field14-1" name="Field14-1" type="tel" class="field text" value="" size="3" maxlength="3" tabindex="9" />
+<label for="Field14-1">###</label>
+</span>
+<span class="symbol">-</span>
+<span>
+ <input id="Field14-2" name="Field14-2" type="tel" class="field text" value="" size="4" maxlength="4" tabindex="10" />
+<label for="Field14-2">####</label>
+</span>
+</li>
+<li id="foli15" class="notranslate      ">
+<label class="desc" id="title15" for="Field15">
+Email
+<span id="req_15" class="req">*</span>
+</label>
+<div>
+<input id="Field15" name="Field15" type="email" spellcheck="false" class="field text medium" value="" maxlength="255" tabindex="11"       required />
+</div>
+</li>
+<li id="foli16"
+class="notranslate      "><label class="desc" id="title16" for="Field16">
+What are the pick-up and delivery locations? <br />
+(City, State, ZIP):
+<span id="req_16" class="req">*</span>
+</label>
+
+<div>
+<textarea id="Field16"
+name="Field16"
+class="field textarea medium"
+spellcheck="true"
+rows="10" cols="50"
+
+tabindex="12"
+onkeyup=""
+      required  ></textarea>
+
+</div>
+</li>
+<li id="foli17"
+class="notranslate      "><label class="desc" id="title17" for="Field17">
+What are the transit deadlines, if any, and when is the shipment available for initial pick up?
+</label>
+
+<div>
+<textarea id="Field17"
+name="Field17"
+class="field textarea medium"
+spellcheck="true"
+rows="10" cols="50"
+
+tabindex="13"
+onkeyup=""
+       ></textarea>
+
+</div>
+</li>
+<li id="foli18"
+class="notranslate      "><label class="desc" id="title18" for="Field18">
+Please describe the items that you would like transported, including sizes and weights and if the items are packed and/or crated:
+<span id="req_18" class="req">*</span>
+</label>
+
+<div>
+<textarea id="Field18"
+name="Field18"
+class="field textarea medium"
+spellcheck="true"
+rows="10" cols="50"
+
+tabindex="14"
+onkeyup=""
+      required  ></textarea>
+
+</div>
+</li>
+<li id="foli19"
+class="notranslate      "><label class="desc" id="title19" for="Field19">
+Please describe both the pick up and delivery location facilities (loading dock, elevators, stairs, etc). Are there any truck size restrictions at either the pick up or delivery locations?
+</label>
+
+<div>
+<textarea id="Field19"
+name="Field19"
+class="field textarea medium"
+spellcheck="true"
+rows="10" cols="50"
+
+tabindex="15"
+onkeyup=""
+       ></textarea>
+
+</div>
+</li>
+<li id="foli20"
+class="notranslate      "><label class="desc" id="title20" for="Field20">
+What type of service are you looking for: Crate and Freight, Shuttle, Last On-First Off, Expedited, Exclusive Use?
+</label>
+
+<div>
+<textarea id="Field20"
+name="Field20"
+class="field textarea medium"
+spellcheck="true"
+rows="10" cols="50"
+
+tabindex="16"
+onkeyup=""
+       ></textarea>
+
+</div>
+</li><li id="foli21" class="notranslate      ">
+<fieldset>
+<![if !IE | (gte IE 8)]>
+<legend id="title21" class="desc">
+Do you need transportation insurance?
+<span id="req_21" class="req">*</span>
+</legend>
+<![endif]>
+<!--[if lt IE 8]>
+<label id="title21" class="desc">
+Do you need transportation insurance?
+<span id="req_21" class="req">*</span>
+</label>
+<![endif]-->
+<div>
+<input id="radioDefault_21" name="Field21" type="hidden" value="" />
+<span>
+<input id="Field21_0" name="Field21" type="radio" class="field radio" value="Yes" tabindex="17" checked="checked"     required />
+<label class="choice" for="Field21_0" >
+Yes</label>
+</span>
+<span>
+<input id="Field21_1" name="Field21" type="radio" class="field radio" value="No" tabindex="18"     required />
+<label class="choice" for="Field21_1" >
+No</label>
+</span>
+</div>
+</fieldset>
+</li>
+<li id="foli22"
+class="notranslate      "><label class="desc" id="title22" for="Field22">
+How do you prefer to be contacted? Phone, fax, or email? Is there a best time to reach you?
+</label>
+
+<div>
+<textarea id="Field22"
+name="Field22"
+class="field textarea medium"
+spellcheck="true"
+rows="10" cols="50"
+
+tabindex="19"
+onkeyup=""
+       ></textarea>
+
+</div>
+</li>
+<li id="foli23" class="notranslate      ">
+<label class="desc" id="title23" for="Field23">
+How did you hear about us?
+</label>
+<div>
+<input id="Field23" name="Field23" type="text" class="field text medium" value="" maxlength="255" tabindex="20" onkeyup=""       />
+</div>
+</li><li id="foli25" class="notranslate      ">
+<fieldset>
+<![if !IE | (gte IE 8)]>
+<legend id="title25" class="desc">
+Who would you like to receive this request?
+<span id="req_25" class="req">*</span>
+</legend>
+<![endif]>
+<!--[if lt IE 8]>
+<label id="title25" class="desc">
+Who would you like to receive this request?
+<span id="req_25" class="req">*</span>
+</label>
+<![endif]-->
+<div>
+<input id="radioDefault_25" name="Field25" type="hidden" value="" />
+<span>
+<input id="Field25_0" name="Field25" type="radio" class="field radio" value="Rocky Mountain / Denver Office" tabindex="21"     required />
+<label class="choice" for="Field25_0" >
+Rocky Mountain / Denver Office</label>
+</span>
+<span>
+<input id="Field25_1" name="Field25" type="radio" class="field radio" value="California / San Francisco Office" tabindex="22" checked="checked"     required />
+<label class="choice" for="Field25_1" >
+California / San Francisco Office</label>
+</span>
+</div>
+</fieldset>
+</li> <li class="buttons ">
+<div>
+
+                    <input id="saveForm" name="saveForm" class="btTxt submit"
+    type="submit" value="Submit"
+ /></div>
+</li>
+
+<li class="hide">
+<label for="comment">Do Not Fill This Out</label>
+<textarea name="comment" id="comment" rows="1" cols="1"></textarea>
+<input type="hidden" id="idstamp" name="idstamp" value="MIJKil5dIdXzo3Fk6bzVVjBt+QQ8LjK1OU/EkIY88Ok=" />
+</li>
+</ul>
+</form>
+
 
   </div> <!-- /main -->
 
